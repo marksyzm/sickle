f2611a7ee5010efcc914a50018a8f6ec505bb093
Mark Elphinstone-Hoadley <mark@midair.info>
2015-03-25 10:48:47 +0000

    Increased the timeout on mocha:istanbul

--- tasks/mocha_istanbul.js
+++ tasks/mocha_istanbul.js
@@ -7,12 +7,14 @@ module.exports = function mocha_istanbul(grunt) {
         coverage: {
             src: "test",
             options: {
+                timeout: 6000,
                 coverage:true
             }
         },
         coveralls: {
             src: "test",
             options: {
+                timeout: 6000,
                 coverage:true
             }
         }
